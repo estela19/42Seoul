ed31d337d9ad4a69cffc5726c8972375191902bd
estela19 <estela19@DESKTOP-R37KILB.localdomain>
2021-09-25 02:38:49 +0900

    fix files

--- C10/ex02/src/utility.c
+++ C10/ex02/src/utility.c
@@ -6,7 +6,7 @@
 /*   By: sooykim <sooykim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 15:27:06 by sooykim           #+#    #+#             */
-/*   Updated: 2021/09/21 22:06:46 by sooykim          ###   ########.fr       */
+/*   Updated: 2021/09/25 02:13:12 by sooykim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ void	print(char *str)
 
 void	noexist_error(char *path)
 {
-	print ("tail: cannot open '");
+	print ("ft_tail: ");
 	print (path);
-	print ("' for reading: ");
+	print (": ");
 	print (strerror(errno));
 	print ("\n");
 }
