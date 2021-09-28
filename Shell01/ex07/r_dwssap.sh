76bb50ec6d2b9805baca9836d3d1494d7947f059
estela19 <estela19@DESKTOP-R37KILB.localdomain>
2021-09-29 02:29:31 +0900

    fix shell01

--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat /etc/passwd | sed '/^#/d' | sed -n 'n;p' | awk -F":" '{print $1}' | rev | sort -r | sed -n $FT_LINE1''$FT_LINE2' p' | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/, /g' -e 's/$/./' | tr -d '\n'
+cat /etc/passwd | sed '/^#/d' | sed -n 'n;p' | awk -F":" '{print $1}' | rev | sort -r |  awk -v FT_LINE1="$FT_LINE1" -v FT_LINE2="$FT_LINE2"  'NR >= FT_LINE1 && NR <= FT_LINE2 { print $0 }' | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/, /g' -e 's/$/./' | tr -d '\n'
